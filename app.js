bfaa5bfe85a87a0fbf9274f898f71789f651e376
Chittepu Varshini <varshinireddy963@gmail.com>
2023-10-25 18:43:39 -0500

    replace index

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
-
+var computationRouter = require('./routes/computation');
 var app = express();
 
 // view engine setup
@@ -20,7 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/mydata', mydataRouter);
 app.use('/users', usersRouter);
+app.use('/computation', computationRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
