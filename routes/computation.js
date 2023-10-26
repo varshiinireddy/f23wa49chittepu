666f0901fcb2c59b3120be1a264df1af8292be0b
Chittepu Varshini <varshinireddy963@gmail.com>
2023-10-25 20:18:46 -0500

    modified

--- routes/computation.js
+++ routes/computation.js
@@ -1,20 +1,20 @@
 var express = require('express');
 var router = express.Router();
-var atan2=0;
-var atanh =0;
+var sin=0;
+var sin =0;
 var cbrt =0
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     var x= Math.random();
     var y = Math.random();
-    atan2 = Math.atan2(x,y);
-    atanh = Math.atanh(x);
+    sin = Math.sin(x,y);
+    sin = Math.sin(x);
     cbrt = Math.cbrt(x);
-    //res.render('computation', {title: `Math.atan2(x,y) applied to ` + {x} +  + `is` + {atan2}  });
-    //res.render('Math.atan2(x,y) applied to ' + x   + 'is' + atan2  );
+    //res.render('computation', {title: `Math.sin(x,y) applied to ` + {x} +  + `is` + {sin}  });
+    //res.render('Math.sin(x,y) applied to ' + x   + 'is' + sin  );
     res.setHeader('Content-Type', 'text/html');
 
-    res.send(  `f(atan2(${x},${y})) is ${atan2}`);
+    res.send(  `f(sin(${x},${y})) is ${sin}`);
   
 });
 
